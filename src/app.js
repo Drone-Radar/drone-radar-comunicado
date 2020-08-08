94aa901e82c413dc895d40ad3373f46ad9c91780
Lucas Gabriel <damascena.lucasg@gmail.com>
2020-08-08 20:44:08 -0300

    Commit Nodemailer

--- src/app.js
+++ src/app.js
@@ -1,34 +1,35 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const cors= require('cors');
+const cors = require('cors');
 const requireDir = require('require-dir');
 const amqpService = require('./app/services/AmqpService');
+const { validateData } = require('./app/services/HistoricoService');
 
-class AppController{
-    constructor(){
-        const app = express()
-        app.use(express.json())
-        app.use(cors());
+class AppController {
+	constructor() {
 
-        require('dotenv/config');
+		const app = express()
+		app.use(express.json())
+		app.use(cors());
 
-        this.database()        
-        requireDir('./app/models');
+		require('dotenv/config');
 
-        amqpService.start();
+		this.database()
+		requireDir('./app/models');
 
-        return app
-    }
+		amqpService.start();
 
-    database(){
-        mongoose.connect(process.env.MONGODB_CONNECTIONSTRING,
-            { 
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        );
-        
-    }
+		setInterval(validateData, (60000));
+
+		return app;
+	}
+
+	database() {
+		mongoose.connect(process.env.MONGODB_CONNECTIONSTRING, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+	}
 }
 
 module.exports = new AppController()
